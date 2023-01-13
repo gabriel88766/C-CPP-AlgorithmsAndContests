d8b88a78c7acc2338449946a75e02d9601ee8c3f
Gabriel Vinícius de Souza Silva <gabriel.souzasilva125@gmail.com>
2023-01-13 11:53:37 -0300

    raddix_sort

--- Codeforces/EDU/ITMOAcademy/SuffixArray/raddix_sort.cpp
+++ Codeforces/EDU/ITMOAcademy/SuffixArray/raddix_sort.cpp
@@ -7,13 +7,15 @@ using namespace std;
 const int N = 2e5;
 int ans[N], cnt[N], aux[N];
 
-void raddix_sort(vector<pair<int,int>> &v){
-    for(int i=0; i<v.size(); i++) cnt[v[i].second]++;
-    for(int i=1;i<v.size(); i++) cnt[i] += cnt[i-1];
-    for(int i=0;i<v.size();i++) aux[--cnt[v[i].second]] = i;
-
-    
-    
+void raddix_sort(vector<pair<int,int>> &v, int bounds){ //for pairs
+    int n = v.size();
+    for(int i=0; i<n; i++) cnt[v[i].second+1]++; //strategy, bounds next element
+    for(int i=2; i<=(bounds+1); i++) cnt[i] += cnt[i-1];
+    for(int i=0;i<n;i++) aux[cnt[v[i].second]++] = i;
+    for(int i=0;i<=(bounds+1); i++) cnt[i] = 0;
+    for(int i=0; i<n; i++) cnt[v[i].first+1]++; 
+    for(int i=2; i<=(bounds+1); i++) cnt[i] += cnt[i-1];
+    for(int i=0;i<n;i++) ans[cnt[v[aux[i]].first]++] = aux[i];    
 }
 
 
@@ -29,9 +31,7 @@ int main(){
     cin.tie(NULL);
     //freopen("in", "r", stdin); test input
     vector<pair<int,int>> pairs = {{3, 3}, {2, 1}, {2, 3}, {1, 3}, {1,2}};
-    raddix_sort(pairs);
+    raddix_sort(pairs, 3);
 
-    vector<int> v = {1 ,2, 3, 4, 5}, mp = {4, 2, 3, 0, 1}, invmp = {3, 4, 1, 2, 0};
-    test(v, mp);
-    for(int i=0;i<5;i++) cout << ans[i] << " ";
+    for(int i=0;i<5;i++) cout << ans[i] << " "; //index ordered;
 }
