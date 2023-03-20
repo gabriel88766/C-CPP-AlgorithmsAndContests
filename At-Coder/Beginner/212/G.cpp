131957cb1d5b2034c38897e33e08699c1a24fc70
Gabriel Vinícius de Souza Silva <gabriel.souzasilva125@gmail.com>
2023-03-20 16:21:13 -0300

    primitiveroot problem

--- At-Coder/Beginner/212/G.cpp
+++ At-Coder/Beginner/212/G.cpp
@@ -11,5 +11,33 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     //freopen("in", "r", stdin); //test input
-    
+    ll p;
+    cin >> p;
+    ll an = p-1;
+    ll aux = an;
+    vector<ll> div, f;
+    for(ll i = 1; i*i <= an; i++){
+        if(!(an%i)){
+            if(i != 1){
+                if(!(aux%i)){
+                    while(!(aux % i)){
+                        aux /= i;
+                    }
+                    f.push_back(i);
+                }
+            }
+            div.push_back(i);
+            if(i*i != an) div.push_back(an/i);
+        }
+    }
+    if(aux != 1) f.push_back(aux);
+    sort(div.begin(), div.end());
+    ll ans = 1;
+    for(auto x: div){
+        aux = an/x;
+        ll phi = aux;
+        for(auto y : f) if(!(phi%y)) phi -= phi/y;
+        ans = (ans + (aux%MOD)*(phi%MOD)) % MOD;
+    }
+    cout << ans;
 }
