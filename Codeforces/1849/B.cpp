cc1f2c397c7da6249f88d33ed5e1a2d33a254440
Gabriel Vinícius de Souza Silva <gabriel.souzasilva125@gmail.com>
2023-07-27 16:24:08 -0300

    .

---
+++ Codeforces/1849/B.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+typedef unsigned long long int ull;
+const ll INF_LL = 0x3f3f3f3f3f3f3f3f, MOD = 998244353; //1e9+7
+const int INF_INT = 0x3f3f3f3f;
+const long double PI = acosl(-1.), EPS = 1e-9; 
+using namespace std;
+
+//cout << fixed << setprecision(6)
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //freopen("in", "r", stdin); //test input
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        vector<int> v(n);
+        for(int i=0;i<n;i++) cin >> v[i];
+        set<pair<int,int>> st;
+        for(int i=0;i<n;i++){
+            v[i] = (v[i] % k);
+            if(v[i] == 0) v[i] += k;
+            st.insert({v[i], -(i+1)});
+        }
+        while(!st.empty()){
+            auto v = *prev(st.end());
+            cout << -v.second << " ";
+            st.erase(prev(st.end()));
+        }
+        cout << "\n";
+    }
+}
