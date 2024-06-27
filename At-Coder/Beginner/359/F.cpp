a7729b90432613c9358d9bad15000e5cfbca83f5
= <=>
2024-06-27 16:38:50 -0300

    .

--- At-Coder/Beginner/359/F.cpp
+++ At-Coder/Beginner/359/F.cpp
@@ -11,5 +11,23 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     //freopen("in", "r", stdin); //test input
-
+    int n;
+    cin >> n;
+    ll ans = 0;
+    multiset<pair<ll, ll>> ms;
+    for(int i=1;i<=n;i++){
+        ll x;
+        cin >> x;
+        ms.insert({3*x, 3});
+        ans += x;
+    }
+    // n, missing n-2;
+    for(int i=0;i<n-2;i++){
+        auto [val, qt] = *ms.begin();
+        ans += val;
+        ll nv = (val/qt)*(qt+2);
+        ms.erase(ms.begin());
+        ms.insert({nv, qt+2});
+    }
+    cout << ans << "\n";
 }
