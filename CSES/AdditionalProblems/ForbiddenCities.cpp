96f971707b292385dca5d30d22eddc19c781ef08
= <=>
2024-09-17 16:27:47 -0300

    1

--- CSES/AdditionalProblems/ForbiddenCities.cpp
+++ CSES/AdditionalProblems/ForbiddenCities.cpp
@@ -9,7 +9,7 @@ using namespace std;
 const int N = 1e5+2;
 vector<int> adj[N];
 int ans[N], artp[N], low[N], in[N];
-set<int> st[N];
+set<pair<int,int>> stc[N];
 int cnt;
 /*struct Query{
     int l, r, j;
@@ -24,8 +24,10 @@ void dfs_artp(int u, int p = -1){
         if(!in[v]){
             dfs_artp(v, u);
             if(low[v] >= in[u] && p != -1){
-                st[u].insert(low[v]);
                 artp[u] = true;
+            }
+            if(low[v] >= in[u]){
+                stc[u].insert({cnt, in[v]});
             }
             ch++;
             low[u] = min(low[u], low[v]);
@@ -50,17 +52,25 @@ int main(){
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    adj[0].push_back(1);
-    adj[1].push_back(0);
-    dfs_artp(0);
+    dfs_artp(1);
     for(int i=0;i<q;i++){
         int l, r, c;
         cin >> l >> r >> c;
-        if(!artp[c]) cout << "YES\n";
+        if(l == c || r == c) cout << "NO\n";
+        else if(!artp[c]) cout << "YES\n";
         else{
-            auto it1 = st[c].upper_bound(in[l]);
-            auto it2 = st[c].upper_bound(in[r]);
-            if(it1 == it2) cout << "YES\n";
+            auto it1 = stc[c].upper_bound({in[l], 0});
+            auto it2 = stc[c].upper_bound({in[r], 0});
+            bool o1, o2;
+            if(it1 == stc[c].end() || it1->second > in[l]) o1 = true;
+            else o1 = false;
+            if(it2 == stc[c].end() || it2->second > in[r]) o2 = true;
+            else o2 = false;
+
+            if(o1 || o2){
+                if(o1 != o2) cout << "NO\n";
+                else cout << "YES\n";
+            }else if(it1 == it2) cout << "YES\n";
             else cout << "NO\n";
         }
     }
